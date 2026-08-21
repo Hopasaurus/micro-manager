b8b6ca8db796596adfa8ff309cddc4ed1421d666
David Hoppe <hopasaurus@gmail.com>
2026-08-21 14:09:17 -0400

    Bump CLI version to 0.2.0

--- implementations/golang/internal/cli/cli.go
+++ implementations/golang/internal/cli/cli.go
@@ -27,7 +27,7 @@ const (
 
 // Version is the tool version, and the format spec version it implements.
 const (
-	Version           = "0.1.0"
+	Version           = "0.2.0"
 	FormatSpecVersion = "1"
 )
 
